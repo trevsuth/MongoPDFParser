b237ab7f041b41ed600f07c95ecf414c1dd27d3e
trevsuth <trevor.sutherland@gmail.com>
2024-04-06 22:45:38 -0400

    update gitignore

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from util.pdf import parse_pdf
+from util.llm import embed
 
-file_path = 'DataSets/technical-pb-mini-split-inverter-60hz-4myw15-sn-07092021.pdf'
-parse_pdf(file_path)
+# test parse_pdf
+# file_path = 'DataSets/technical-pb-mini-split-inverter-60hz-4myw15-sn-07092021.pdf'
+# parse_pdf(file_path)
+
+# test embed function
+# text = 'hello there, bill'
+# embedded_text = embed(text)
+# print(embedded_text)
